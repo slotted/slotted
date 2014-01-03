0279823a43d73f92da2ca40536c45bd781568e62
jkleiss <jeffg.npc@gmail.com>
2014-01-03 11:45:45 -0800

    * Added Javadoc

--- src/com/googlecode/slotted/client/SlottedActivity.java
+++ src/com/googlecode/slotted/client/SlottedActivity.java
@@ -21,7 +21,11 @@ import com.google.gwt.place.shared.Place;
 import com.google.gwt.user.client.ui.AcceptsOneWidget;
 import com.google.web.bindery.event.shared.EventBus;
 
-//todo javadoc
+/**
+ * Implemented by other objects to control a portion of the UI, and the life cycle is controlled
+ * by the {@link SlottedController}.  Extends {@link Activity}, by adding methods to handle the
+ * Slotted Place hierarchy, and some convenience methods.
+ */
 abstract public class SlottedActivity extends AbstractActivity {
 
     private SlottedController slottedController;
@@ -37,6 +41,19 @@ abstract public class SlottedActivity extends AbstractActivity {
         start(panel);
     }
 
+    /**
+     * Called when the Activity should ready its widget for the user.
+     *
+     * There are three ways to handle display of Activity's widget.  The first is to set the
+     * activity's widget in the panel and call {@setLoadingStarted} which will activate Delayed Loading
+     * (https://code.google.com/p/slotted/wiki/DelayedLoading).  Second is to set the activity's widget
+     * to display correctly while loading.  Third is to delay the setting the of the activity's widget
+     * until it is complete, which will won't prevent the rest of the hierarchy from being displayed.
+     *
+     * The event bus can be retrieved from {@link #getEventBus()}.
+     *
+     * @param panel the panel to display this activity's widget
+     */
     abstract public void start(AcceptsOneWidget panel);
 
 
@@ -50,12 +67,23 @@ abstract public class SlottedActivity extends AbstractActivity {
                 "the SlottedActivity overrides getChildSlotDisplay()");
     }
 
+    /**
+     * Called be SlottedController to get the Widget that will display the child slot.
+     *
+     * @param slot The child Slot defined in the SlottedPlace.
+     * @return The widget that will display the child Activity's widget.  This activity must attach
+     * the widget to DOM to be displayed correctly.
+     */
+    @SuppressWarnings("deprecation")
     public AcceptsOneWidget getChildSlotDisplay(Slot slot) {
         setChildSlotDisplay(slot);
         return slot.getDisplay();
     }
 
-    public void init(SlottedController slottedController, SlottedPlace currentPlace,
+    /**
+     * Called by SlottedController pass all the values needed for the convience methods.
+     */
+    protected void init(SlottedController slottedController, SlottedPlace currentPlace,
             PlaceParameters placeParameters, EventBus eventBus, ActiveSlot activeSlot)
     {
         this.slottedController = slottedController;
@@ -75,48 +103,95 @@ abstract public class SlottedActivity extends AbstractActivity {
         onStop();
     }
 
+    /**
+     * Called when the SlottedController#goTo() is called and the current activity doesn't change.
+     * This happens when a Child Activity is navigated to and it has the same parent, or the goTo() is
+     * called with a new Place that equals the existing Place.
+     */
     public void onRefresh() {
     }
 
+    /**
+     * Called when loading process is complete.  The widget is not added to the DOM until the loading is
+     * complete, so this method allows code to be run after the activity's widgets are added to the DOM.
+     */
     public void onLoadComplete() {
     }
 
+    /**
+     * Activates the Delayed Loading if called inside the {@link #start(AcceptsOneWidget)} method.  If called
+     * outside the start(), then a LoadingEvent is sent, but Slotted lifecycle is unaffected.
+     */
     public void setLoadingStarted() {
         activeSlot.setLoading(true, this);
     }
 
+    /**
+     * Notifies the Delayed Loading code to attempt to display the page.  If another page in the hierarchy is
+     * loading, this call will not trigger the widgets to be displayed.  If delayed loading is not active, then a
+     * LoadingEvent with be sent stating loading is complete.
+     */
     public void setLoadingComplete() {
         activeSlot.setLoading(false, this);
     }
 
+    /**
+     * Convenience method for getting the SlottedController
+     */
     public SlottedController getSlottedController() {
         return slottedController;
     }
 
+    /**
+     * See {@link SlottedController#getCurrentPlace(Class)}
+     */
     public <T extends Place> T getCurrentPlace(Class<T> type) {
         return slottedController.getCurrentPlace(type);
     }
 
+    /**
+     * See {@link SlottedController#getCurrentActivity(Class)}
+     */
     public <T extends Activity> T getCurrentActivity(Class<T> type) {
         return slottedController.getCurrentActivity(type);
     }
 
+    /**
+     * See {@link SlottedController#getCurrentActivityByPlace(Class)}
+     */
     public Activity getCurrentActivityByPlace(Class<? extends Place> type) {
         return slottedController.getCurrentActivityByPlace(type);
     }
 
+    /**
+     * Gets the SlottedPlace that created this activity.
+     */
     public SlottedPlace getCurrentPlace() {
         return currentPlace;
     }
 
+    /**
+     * Convenience method for getting the PlaceParameters which represent all the gobal parameters.
+     */
     public PlaceParameters getPlaceParameters() {
         return placeParameters;
     }
 
+    /**
+     * Convenience method for getting the EventBus.
+     *
+     * Any handlers attached to the event bus will be de-registered when
+     * the activity is stopped, so activities will rarely need to hold on to the
+     * {@link com.google.gwt.event.shared.HandlerRegistration HandlerRegistration}
+     * instances returned by {@link EventBus#addHandler}.
+     */
     public EventBus getEventBus() {
         return eventBus;
     }
 
+    /**
+     * @return The simple class name.
+     */
     @Override
     public String toString() {
         return this.getClass().getName().substring(this.getClass().getName().lastIndexOf(".") + 1);
--- src/com/googlecode/slotted/client/SlottedException.java
+++ src/com/googlecode/slotted/client/SlottedException.java
@@ -1,20 +1,34 @@
 package com.googlecode.slotted.client;
 
 /**
- * todo javadoc
+ * A wrapper for exceptions that happen while the SlottedController is processing.
+ * This is similar to the GWT UmbrellaExceptions, which is necessary to handle non Runtime Exceptions.
  */
 public class SlottedException extends RuntimeException {
     protected SlottedException() {
     }
 
+    /**
+     * Creates a exception with a specific message.
+     */
     public SlottedException(String s) {
         super(s);
     }
 
+    /**
+     * Creates an exception with a root cause.
+     * @param e
+     */
     protected SlottedException(Throwable e) {
         super(e);
     }
 
+    /**
+     * Wraps exceptions to make all exceptions RuntimeExceptions.
+     *
+     * @param e The exception to wrap.
+     * @return SlottedException that is ready to be thrown.
+     */
     public static SlottedException wrap(Throwable e) {
         if (e instanceof SlottedException) {
             return (SlottedException) e;
