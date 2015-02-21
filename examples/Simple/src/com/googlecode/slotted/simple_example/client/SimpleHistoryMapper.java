c7b39af639a8f56435d22adecdc2be15a7946f50
jkleiss <jeffg.npc@gmail.com>
2015-02-21 15:38:11 -0500

    * Added package filtering for AutoHistoryMapper

---
+++ examples/Simple/src/com/googlecode/slotted/simple_example/client/SimpleHistoryMapper.java
@@ -0,0 +1,8 @@
+package com.googlecode.slotted.simple_example.client;
+
+import com.googlecode.slotted.client.AutoHistoryMapper;
+import com.googlecode.slotted.client.ScanPackages;
+
+@ScanPackages("com.googlecode.slotted.simple_example.client.place")
+abstract public class SimpleHistoryMapper extends AutoHistoryMapper {
+}
