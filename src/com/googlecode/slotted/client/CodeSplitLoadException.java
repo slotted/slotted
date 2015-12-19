8af9e451ba184c6889dd8f2b6b2044c6bc5c915e
jkleiss <jeffg.npc@gmail.com>
2015-12-19 10:26:11 -0500

    * Added CodeSplitLoadException

---
+++ src/com/googlecode/slotted/client/CodeSplitLoadException.java
@@ -0,0 +1,10 @@
+package com.googlecode.slotted.client;
+
+public class CodeSplitLoadException extends RuntimeException {
+	public CodeSplitLoadException() {
+	}
+
+	public CodeSplitLoadException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
